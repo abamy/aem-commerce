3ebcf17743120c2311cdf94eed558504d6d0db96
Arthur Bamy <arthur.bamy@gmail.com>
2024-08-29 13:21:48 +0200

    update dropins version and scripts

--- scripts/__dropins__/storefront-cart/components/index.d.ts
+++ scripts/__dropins__/storefront-cart/components/index.d.ts
@@ -1,4 +1,8 @@
-export * from './EmptyCart';
 export * from './Cart';
+export * from './EmptyCart';
+export * from './EstimateShipping';
 export * from './MiniCart';
+export * from './CartSummaryGrid';
+export * from './CartSummaryList';
+export * from './OrderSummary';
 //# sourceMappingURL=index.d.ts.map
